86408d243bf8631623f6f5221aec0c4a44f26ada
Joseph Fernando <srikanthfernando3@gmail.com>
2025-11-05 11:21:51 +0800

    Refactor layout and page components: update metadata, remove image, and simplify page structure

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function NavBar() {
+    return (
+        <div>
+            <nav className="fixed flex top-0 justify-between p-6 w-full z-50">
+                <div>
+                    <div>
+                        <Image src="/PixelFactoryLogo.svg" alt="" width={100} height={100} 
+                        />
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
---
+++ app/pages/HomePage.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function HomePage() {
+    return (
+        <div className="relative h-screen w-full overflow-hidden">
+            {/* Background Video */}
+            <video
+                autoPlay
+                loop
+                muted
+                playsInline
+                className="absolute top-0 left-0 w-full h-full object-cover"
+            >
+                <source src="./PixelFactory.webm" type="video/webm" />
+                Your browser does not support the video tag.
+            </video>
+
+            {/* Dark Overlay */}
+            <div className="absolute inset-0 bg-black/50"></div>
+
+            {/* Page Content */}
+            <div className="relative flex items-center justify-center h-full">
+                <div className='grid grid-cols-1'>
+                    <div>
+                    <Image src="/WelcomePixelFactory.svg" alt="" width={1920} height={1080} />
+                    </div>
+                    <div>
+                        <Image src="/ExploreServices.svg" alt="" width={100} height={100} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
