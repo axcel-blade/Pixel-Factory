0c6f65505c21e26f53c3e4b7db45aaf72bf3a5fd
Joseph Fernando <srikanthfernando3@gmail.com>
2026-05-18 12:24:57 +0800

    Fix false hydration warnings from Grammarly on root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,9 +35,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} font-poppins antialiased`}
+        suppressHydrationWarning
       >
         <JsonLd />
         {children}
